5b8e2d896ce173b1b2d609bc497daf790e7856a8
AkshataHasabi <akshuh818@gmail.com>
2022-05-22 22:21:28 +0530

    ADDED:created welcome message

---
+++ src/main/java/com/example/employeepayrollapp/EmployeePayRollAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRollAppApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to employeepayroll app");
+        SpringApplication.run(EmployeePayRollAppApplication.class, args);
+    }
+
+}
